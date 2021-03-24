0eb6d269eaab7bd89e4fcdeaa7712d8fbb5503cb
EdoardoCamm <79929557+EdoardoCamm@users.noreply.github.com>
2021-03-24 13:12:18 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    Case += 1
+    if (Case == 1) {
+        basic.clearScreen()
+    } else if (Case == 2) {
+        basic.showString("GYR")
+    } else if (Case == 3) {
+        basic.showString("GYR+walk")
+    } else if (Case == 4) {
+        basic.showString("YL+RD")
+    } else {
+    	
+    }
+})
+let Case = 0
+Case = 0
